c63d1d9de21c0f952b0e0d39cef5bd00141a5e2a
Haseo97 <jawgattus@gmail.com>
2018-05-08 19:25:53 +0700

    santoni: Enable SDclang usage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# SDclang usage
+TARGET_USE_SDCLANG := true
+
 TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
 
